dcf5742d7a3bbb1ef5cd052ab69e40d29b1b3c88
Juan Navarro <jsnavarroa@unal.edu.co>
2017-03-24 07:15:05 -0500

    Create test case for ImageCloneGA class

--- src/imageclonega/ImageCloneGA.java
+++ src/imageclonega/ImageCloneGA.java
@@ -31,117 +31,160 @@ import unalcol.types.real.array.DoubleArray;
 import unalcol.types.real.array.DoubleArrayPlainWrite;
 
 public class ImageCloneGA {
-	
-	
 
-		public static void schemeTheorem() {
-			// Search Space definition
-			int DIM = 24;
-			Space<BitArray> space = new BinarySpace(DIM);
+	public static void schemeTheorem() {
+		// Search Space definition
+		int DIM = 24;
+		Space<BitArray> space = new BinarySpace(DIM);
 
-			// Optimization Function
-			OptimizationFunction<BitArray> function = new MaxOnes();
-			Goal<BitArray, Double> goal = new OptimizationGoal<BitArray>(function,
-					false); // maximizing, remove the parameter false if minimizing
+		// Optimization Function
+		OptimizationFunction<BitArray> function = new MaxOnes();
+		Goal<BitArray, Double> goal = new OptimizationGoal<BitArray>(function,
+				false); // maximizing, remove the parameter false if minimizing
 
-			// Sel method
-			int POPSIZE = 100;
-			Selection<BitArray> selection = new Roulette<BitArray>();
+		// Sel method
+		int POPSIZE = 100;
+		Selection<BitArray> selection = new Roulette<BitArray>();
 
-			BitArray[] pop = space.pick(POPSIZE);
+		BitArray[] pop = space.pick(POPSIZE);
 
-			// TODO create a for with pick
-			@SuppressWarnings("unchecked")
-			Solution<BitArray>[] solution = new Solution[pop.length];
-			for (int i = 0; i < pop.length; i++) {
-				solution[i] = new Solution<BitArray>(pop[i], goal);
-			}
-
-			int MAX_ITER = 100;
-
-			for (int i = 0; i < MAX_ITER; i++) {
-				solution = selection.pick(POPSIZE, solution);
-				if (i % 10 == 0) {
-					System.out.println("##### " + i + " ####");
-					histogramHashtable(solution);
-				}
-			}
-
+		// TODO create a for with pick
+		@SuppressWarnings("unchecked")
+		Solution<BitArray>[] solution = new Solution[pop.length];
+		for (int i = 0; i < pop.length; i++) {
+			solution[i] = new Solution<BitArray>(pop[i], goal);
 		}
 
-		private static int[] histogram(BitArray[] pop) {
-			int[] h = new int[pop.length];
-			for (int i = 0; i < pop.length; i++) {
-				for (int j = i + 1; j < h.length; j++) {
-					if (pop[i].equals(pop[j])) {
-						h[i]++;
-					}
-				}
+		int MAX_ITER = 100;
+
+		for (int i = 0; i < MAX_ITER; i++) {
+			solution = selection.pick(POPSIZE, solution);
+			if (i % 10 == 0) {
+				System.out.println("##### " + i + " ####");
+				histogramHashtable(solution);
 			}
-			return h;
-		}
-
-		private static Hashtable<Solution<BitArray>, Integer> histogramHashtable(
-				Solution<BitArray>[] pop) {
-			Hashtable<Solution<BitArray>, Integer> hash = new Hashtable<Solution<BitArray>, Integer>();
-			for (int i = 0; i < pop.length; i++) {
-				Integer k = hash.get(pop[i]);
-				hash.put(pop[i], k != null ? (k + 1) : 1);
-			}
-
-			// Imprimir
-			for (Entry<Solution<BitArray>, Integer> entry : hash.entrySet()) {
-				System.out.println(String.format("%d: %s", entry.getKey(),
-						entry.getValue()));
-			}
-
-			return hash;
-		}
-
-		public static void binary() {
-			// Search Space definition
-			int DIM = 24;
-			Space<BitArray> space = new BinarySpace(DIM);
-
-			// Optimization Function
-			OptimizationFunction<BitArray> function = new MaxOnes();
-			Goal<BitArray, Double> goal = new OptimizationGoal<BitArray>(function,
-					false); // maximizing, remove the parameter false if minimizing
-
-			// Variation definition
-			Variation_1_1<BitArray> mutation = new BitMutation();
-			XOver xover = new XOver();
-
-			// Search method
-			int POPSIZE = 100;
-			int MAXITERS = 100;
-			EAFactory<BitArray> factory = new EAFactory<BitArray>();
-			PopulationSearch<BitArray, Double> search = factory.generational_ga(
-					POPSIZE, new Roulette<BitArray>(), mutation, xover, 0.0,
-					MAXITERS);
-
-			// Tracking the goal evaluations
-			WriteDescriptors write_desc = new WriteDescriptors();
-			Write.set(double[].class, new DoubleArrayPlainWrite(false));
-			Write.set(Population.class, write_desc);
-			Descriptors
-					.set(Population.class, new PopulationDescriptors<BitArray>());
-
-			ConsoleTracer tracer = new ConsoleTracer();
-			// Tracer.addTracer(goal, tracer); // Uncomment if you want to trace the
-			// function evaluations
-			Tracer.addTracer(search, tracer); // Uncomment if you want to trace the
-												// hill-climbing algorithm
-
-			// Apply the search method
-			Solution<BitArray> solution = search.solve(space, goal);
-
-			System.out.println(solution.info(Goal.class.getName()));
-			// Remove for general use
-			Glovito g = new Glovito(solution.object());
-			System.out.println(g.toString());
 		}
 
 	}
 
+	private static int[] histogram(BitArray[] pop) {
+		int[] h = new int[pop.length];
+		for (int i = 0; i < pop.length; i++) {
+			for (int j = i + 1; j < h.length; j++) {
+				if (pop[i].equals(pop[j])) {
+					h[i]++;
+				}
+			}
+		}
+		return h;
+	}
 
+	private static Hashtable<Solution<BitArray>, Integer> histogramHashtable(
+			Solution<BitArray>[] pop) {
+		Hashtable<Solution<BitArray>, Integer> hash = new Hashtable<Solution<BitArray>, Integer>();
+		for (int i = 0; i < pop.length; i++) {
+			Integer k = hash.get(pop[i]);
+			hash.put(pop[i], k != null ? (k + 1) : 1);
+		}
+
+		// Imprimir
+		for (Entry<Solution<BitArray>, Integer> entry : hash.entrySet()) {
+			System.out.println(String.format("%d: %s", entry.getKey(),
+					entry.getValue()));
+		}
+
+		return hash;
+	}
+
+	public static void binary() {
+		// Search Space definition
+		int DIM = 24;
+		Space<BitArray> space = new BinarySpace(DIM);
+
+		// Optimization Function
+		OptimizationFunction<BitArray> function = new MaxOnes();
+		Goal<BitArray, Double> goal = new OptimizationGoal<BitArray>(function,
+				false); // maximizing, remove the parameter false if minimizing
+
+		// Variation definition
+		Variation_1_1<BitArray> mutation = new BitMutation();
+		XOver xover = new XOver();
+
+		// Search method
+		int POPSIZE = 100;
+		int MAXITERS = 100;
+		EAFactory<BitArray> factory = new EAFactory<BitArray>();
+		PopulationSearch<BitArray, Double> search = factory.generational_ga(
+				POPSIZE, new Roulette<BitArray>(), mutation, xover, 0.0,
+				MAXITERS);
+
+		// Tracking the goal evaluations
+		WriteDescriptors write_desc = new WriteDescriptors();
+		Write.set(double[].class, new DoubleArrayPlainWrite(false));
+		Write.set(Population.class, write_desc);
+		Descriptors
+				.set(Population.class, new PopulationDescriptors<BitArray>());
+
+		ConsoleTracer tracer = new ConsoleTracer();
+		// Tracer.addTracer(goal, tracer); // Uncomment if you want to trace the
+		// function evaluations
+		Tracer.addTracer(search, tracer); // Uncomment if you want to trace the
+											// hill-climbing algorithm
+
+		// Apply the search method
+		Solution<BitArray> solution = search.solve(space, goal);
+
+		System.out.println(solution.info(Goal.class.getName()));
+		// Remove for general use
+		Glovito g = new Glovito(solution.object());
+		System.out.println(g.toString());
+	}
+
+	public void binaryImageClone() {
+		// Search Space definition
+		int DIM = 24;
+		Space<BitArray> space = new BinarySpace(DIM);
+
+		// Optimization Function
+		OptimizationFunction<BitArray> function = new SimilariyCheck();
+		Goal<BitArray, Double> goal = new OptimizationGoal<BitArray>(function,
+				false); // maximizing, remove the parameter false if minimizing
+
+		// Variation definition
+		Variation_1_1<BitArray> mutation = new BitMutation();
+		XOver xover = new XOver();
+
+		// Search method
+		int POPSIZE = 100;
+		int MAXITERS = 100;
+		EAFactory<BitArray> factory = new EAFactory<BitArray>();
+		PopulationSearch<BitArray, Double> search = factory.generational_ga(
+				POPSIZE, new Roulette<BitArray>(), mutation, xover, 0.0,
+				MAXITERS);
+
+		// Tracking the goal evaluations
+		WriteDescriptors write_desc = new WriteDescriptors();
+		Write.set(double[].class, new DoubleArrayPlainWrite(false));
+		Write.set(Population.class, write_desc);
+		Descriptors
+				.set(Population.class, new PopulationDescriptors<BitArray>());
+
+		ConsoleTracer tracer = new ConsoleTracer();
+		// Tracer.addTracer(goal, tracer); // Uncomment if you want to trace the
+		// function evaluations
+		Tracer.addTracer(search, tracer); // Uncomment if you want to trace the
+											// hill-climbing algorithm
+
+		// Apply the search method
+		Solution<BitArray> solution = search.solve(space, goal);
+
+		System.out.println("Solution fitness: "
+				+ solution.info(Goal.class.getName()));
+		// Remove for general use
+		Glovito g = new Glovito(solution.object());
+		System.out.println("Solution BitArray: \n" + solution.object());
+
+		System.out.println("Solution BitArray: \n" + g.toString());
+	}
+
+}
