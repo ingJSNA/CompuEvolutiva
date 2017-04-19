8749948c643820bee127fa0eb472565b8d36b56a
unal <unal@unal.edu.co>
2017-04-19 05:20:32 -0500

    Basic tree implementation

---
+++ src/geneticPrograming/GeneticPrograming.java
@@ -0,0 +1,19 @@
+package geneticPrograming;
+
+import java.util.List;
+
+public class GeneticPrograming {
+
+	private List<Node<String>> population;
+	private String[] alphabet = new String[] { "*", "+", "x" };
+
+	private void initializePopulation() {
+
+	}
+
+	public void evolve() {
+		initializePopulation();
+
+	}
+
+}
